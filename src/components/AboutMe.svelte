53d4fb09166f2db66f6808146aa4a8c6bca8d0fa
Prajna Girish <prajnagirish@Prajnas-MacBook-Air.local>
2023-01-07 14:05:00 +0530

    changes to formatting

--- src/components/AboutMe.svelte
+++ src/components/AboutMe.svelte
@@ -4,16 +4,16 @@
     
     <div class="flex md:flex-row flex-col-reverse">
         <div class="content md:px-8 md:w-[80%]">
-            <p class="text-xl text-justify text-[#424242] mb-4">
+            <p class="text-xl text-[#424242] mb-4">
                 Hey! I am currently an engineer at Amazon, India working in the Transporation Technology Org. I completed my undergraduate degree in 2021 from <a href="https://pes.edu/">PES University</a>, Bangalore, India with a Bachelors in
                 Technology in Computer Science and Engineering and a specialisation in Data Science. 
             </p>
-            <p class="text-xl text-justify text-[#424242]">
+            <p class="text-xl  text-[#424242]">
                 My work at Amazon operates on a massive scale. We're responsible for generating the tracking IDs, labels, manifests and invoices for 35% of Amazon's worldwide traffic.
                 
                 <br>
                 <br>
-                My areas of research interest are mainly NLP - speech & text, applied ML, information visualisation and statistical methods in computing
+                My areas of research interest are mainly NLP - speech & text, applied data science and ML, information visualisation and statistical methods for data
                 <br>
                 <br>
             </p>
